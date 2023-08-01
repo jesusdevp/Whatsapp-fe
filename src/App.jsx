d110fa5f53221ee0b6fb847ee2d1935b04b935aa
Jesus Ramirez <jesusdevelp@gmail.com>
2023-07-31 22:12:21 -0600

    Setup tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { ChatIcon } from "./svg"
-
 
 function App() {
 
   return (
-    <>
-      <h1>Hello whatsapp</h1>
-       <ChatIcon />
-       
-    </>
+    <div className="dark" >
+    </div>
   )
 }
 

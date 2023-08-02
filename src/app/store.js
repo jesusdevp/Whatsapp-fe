c689d3b284b2e3a05376aa92391fb7d74154fa91
Jesus Ramirez <jesusdevelp@gmail.com>
2023-08-02 07:54:58 -0600

    Setup redux and  persist redux

---
+++ src/app/store.js
@@ -0,0 +1,34 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import createFilter from "redux-persist-transform-filter"
+import storage from "redux-persist/lib/storage";
+// slices
+import userSlice from "../features/userSlice";
+
+// saveUserOnlyFilter
+const saveUserOnlyFilter =  createFilter('user', ['user'])
+
+// persist config
+const persistConfig = {
+    key: 'user',
+    storage,
+    whitelist: ['user'],
+    transforms: [saveUserOnlyFilter]
+}
+
+const rootReducer = combineReducers({
+    userLogged: userSlice
+})
+
+const persistedReducer = persistReducer( persistConfig, rootReducer )
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: ( getDefaultMiddleware ) => 
+        getDefaultMiddleware({
+            serializableCheck: false
+    }),
+    devTools: true
+})
+
+export const persistor = persistStore( store )
